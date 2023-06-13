029b73a0ad486cb9969844bc9f247ba4d24f3d5a
Hiep <thanhhiep6920@gmail.com>
2023-06-13 14:20:50 +0200

    Tripple kill: Add ReturnFacilityData

--- functions.py
+++ functions.py
@@ -1,3 +1,7 @@
+import uuid
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 def GetCenterCoordinates(coordinatesData):
     lat_sum=0
     lon_sum=0
@@ -7,3 +11,53 @@ def GetCenterCoordinates(coordinatesData):
     center_lat=round(lat_sum/len(coordinatesData),4)
     center_lon=round(lon_sum/len(coordinatesData),4)
     return [center_lat,center_lon]
+
+def randomUUID(limit):
+    random_uuid = [uuid.uuid4() for _ in range(limit)]
+    return random_uuid
+
+def ReturnFacilityData(driver,facilityID,masterFacilityID,facilityTypesIDs,facilityType):
+        facility={
+            "id":facilityID,
+            "name":"unknown",
+            "address":"unknown",
+            "label":"unknown",
+            "capacity":"unknown",
+            "geometry":"polygon",
+            "geolocation":"unknown",      #center coordinates
+            'facilitytype_id':facilityTypesIDs[facilityType],
+            'valid': True,
+            'startdate':"unknown",
+            'enddate':"unknown",
+            "lastchange":"unknown",
+            "master_facility_id":masterFacilityID,
+            "coordinates":"unknown"
+        }
+        driver.get("https://www.openstreetmap.org/api/0.6/"+facilityID)
+        way= driver.find_element(By.TAG_NAME,"way")
+        facility["lastchange"]=way.get_attribute("timestamp")
+
+        tags=driver.find_elements(By.TAG_NAME,"tag")
+        for tag in tags:
+            key=tag.get_attribute("k")
+            value=tag.get_attribute("v")
+            if(key=="name"):
+                facility["name"]=value
+            if(key=="building" or key=="landuse"):
+                facility["label"]=value
+
+        nodes_coordinates=[]
+        nodes_ids=[]
+        nds=driver.find_elements(By.TAG_NAME,"nd")
+        for node_id in nds:
+            nodes_ids.append(node_id.get_attribute("ref"))
+            
+        for node_id in nodes_ids:
+            driver.get("https://www.openstreetmap.org/api/0.6/node/"+node_id)
+            node=driver.find_element(By.TAG_NAME,"node")
+            nodes_coordinates.append([node.get_attribute("lat"),node.get_attribute("lon")])
+
+        facility["geolocation"]=GetCenterCoordinates(nodes_coordinates)
+        facility["coordinates"]=nodes_coordinates
+        return facility
+        
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 import json
 import os
-from functions import GetCenterCoordinates
+from functions import ReturnFacilityData
 
 # Lấy đường dẫn tuyệt đối của tệp Python hiện tại
 current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -15,6 +15,8 @@ json_file_path_1 = os.path.join(current_directory, 'facilitiesIDs.json')
 # Đọc dữ liệu từ tệp JSON
 with open (json_file_path_1,"r") as file:
     input= json.load(file)
+
+facilityTypesIDs = ["areaTypeID","buildingTypeID"]
 
 driver = webdriver.Chrome()
 
@@ -25,52 +27,9 @@ facilities={
 }
 
 for area in input["areas"]:
-    wayIDs=[]
-    wayIDs.append(area["id"])
+    facilities["facilities"].append(ReturnFacilityData(driver,area["id"],area["id"],facilityTypesIDs,0))
     for buildingID in area["buildingIDs"]:
-        wayIDs.append(buildingID)
-    for wayID in wayIDs:
-        facility={
-            "id":"",
-            "master_facility_id":area["id"],
-            "name":"unknown",
-            "address":"unknown",
-            "label":"unknown",
-            "capacity":"unknown",
-            "geometry":"polygon",
-            "geolocation":"unknown",      #center coordinates
-            "coordinates":"unknown",
-            "lastchange":"unknown"
-        }
-        facility["id"]=wayID
-        driver.get("https://www.openstreetmap.org/api/0.6/"+wayID)
-        way= driver.find_element(By.TAG_NAME,"way")
-        facility["lastchange"]=way.get_attribute("timestamp")
-
-        tags=driver.find_elements(By.TAG_NAME,"tag")
-        for tag in tags:
-            key=tag.get_attribute("k")
-            value=tag.get_attribute("v")
-            if(key=="name"):
-                facility["name"]=value
-            if(key=="building" or key=="landuse"):
-                facility["label"]=value
-
-        nodes_coordinates=[]
-        nodes_ids=[]
-        nds=driver.find_elements(By.TAG_NAME,"nd")
-        for node_id in nds:
-            nodes_ids.append(node_id.get_attribute("ref"))
-            
-        for node_id in nodes_ids:
-            driver.get("https://www.openstreetmap.org/api/0.6/node/"+node_id)
-            node=driver.find_element(By.TAG_NAME,"node")
-            nodes_coordinates.append([node.get_attribute("lat"),node.get_attribute("lon")])
-
-        facility["geolocation"]=GetCenterCoordinates(nodes_coordinates)
-        facility["coordinates"]=nodes_coordinates
-        facilities["facilities"].append(facility)
-
+        facilities["facilities"].append(ReturnFacilityData(driver,buildingID,area["id"],facilityTypesIDs,1))
 
 driver.close()
 
